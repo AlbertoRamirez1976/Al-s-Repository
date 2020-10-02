a2bc913bd4bb6ba312c41c7b70f4976648d39ee9
Alberto E. Ramirez <59782181+AlbertoRamirez1976@users.noreply.github.com>
2020-10-02 11:49:29 -0700

    Add files via upload

--- HW2_PS1_ramirez_a.c
+++ HW2_PS1_ramirez_a.c
@@ -2,7 +2,7 @@
 /* Name : Alberto Ramirez                            */
 /* Student ID : 1186065                              */
 /* Homework 2 Program Set 1                          */
-/* Date: 09/27/20                                    */
+/* Date: 10/01/20                                    */
 /* This Program calculates the expenses of traveling */
 /* from one city to another city.                    */
 /*****************************************************/
@@ -14,11 +14,19 @@
 int main()
 {
 
-     //Declaration
-     int days, meals;
-     char sourceCity, destinationCity, modeofTranspo, hotelType, foodType;
-     double transpoCharges;
 
+     //Constant declaration
+     #define Five* 500
+     #define Three* 300
+     #define Ordinary 100
+     #define Vegetarian 15
+     #define Non-Veg 30
+     #define Continental 10
+
+     //Variable Declaration
+     int days, meals;
+     char sourceCity, sourceCity1[15], destinationCity, destinationCity1[15], modeofTranspo, modeofTranspo1[10], hotelType, hotelType1[10], foodType, foodType1[10];
+     double roomCharges, foodCharges, transpoCharges, totalCharges;
 
      printf("\n");
      printf("\n");
@@ -42,6 +50,17 @@ int main()
      printf("ENTER THE SOURCE CITY:\n\nB for Baltimore\n\nC for Chattanooga\n\nN for Nashville\n\nP for Pasadena\n\n");
      printf("\n");
      scanf("%c", &sourceCity);//Read and store source city
+     switch(sourceCity)
+     {
+         case 'B': case 'b': strcpy(sourceCity1, "Baltimore");
+         break;
+         case 'C': case 'c': strcpy(sourceCity1, "Chattanooga");
+         break;
+         case 'N': case 'n': strcpy(sourceCity1, "Nashville");
+         break;
+         case 'P': case 'p': strcpy(sourceCity1, "Pasadena");
+         break;
+     }
      getchar();
      system("clear");
 
@@ -52,6 +71,17 @@ int main()
      printf("ENTER THE DESTINATION CITY:\n\nD for Denver\n\nM for Madison\n\nC for Clarksville\n\nK for Knoxville\n\n");
      printf("\n");
      scanf("%c", &destinationCity);//Read and store destination city
+     switch(destinationCity)
+     {
+         case 'D': case 'd': strcpy(destinationCity1, "Denver");
+         break;
+         case 'M': case 'm': strcpy(destinationCity1, "Madison");
+         break;
+         case 'C': case 'c': strcpy(destinationCity1, "Clarksville");
+         break;
+         case 'K': case 'k': strcpy(destinationCity1, "Knoxville");
+         break;
+     }
      getchar();
      system("clear");
 
@@ -62,6 +92,15 @@ int main()
      printf("ENTER THE MODE OF TRANSPORT:\n\nA for Air\n\nR for Train\n\nB for Bus\n\n");
      printf("\n");
      scanf("%c", &modeofTranspo);//Read and store mode of transportation
+     switch(modeofTranspo)
+     {
+         case 'A': case 'a': strcpy(modeofTranspo1, "Air");
+         break;
+         case 'T': case 't': strcpy(modeofTranspo1, "Train");
+         break;
+         case 'B': case 'b': strcpy(modeofTranspo1, "Bus");
+         break;
+     }
      getchar();
      system("clear");
 
@@ -72,6 +111,44 @@ int main()
      printf("ENTER THE TYPE OF HOTEL:\n\nF for Five Star\n\nT for Three Star\n\nO for Ordinary\n\n");
      printf("\n");
      scanf("%c", &hotelType);//Read and store hotel information
+     switch(hotelType)
+     {
+         case 'F': case 'f': strcpy(hotelType1, "Five*");
+         break;
+         case 'T': case 't': strcpy(hotelType1, "Three*");
+         break;
+         case 'O': case 'o': strcpy(hotelType1, "Ordinary");
+         break;
+     }
+     getchar();
+     system("clear");
+
+     //Data/Input/Prompt user for meal information
+     printf("\n");
+     printf("\n");
+     printf("\n");
+     printf("ENTER THE TYPE OF FOOD:\n\nV for Veg\n\nN for Non-Veg\n\nC for Continental \n");
+     printf("\n");
+     scanf("%c", &foodType);//Read and store meal information
+      switch(foodType)
+     {
+         case 'V': case 'v': strcpy(foodType1, "Vegetarian");
+         break;
+         case 'N': case 'n': strcpy(foodType1, "Non-Veg");
+         break;
+         case 'C': case 'c': strcpy(foodType1, "Continental");
+         break;
+     }
+     getchar();
+     system("clear");
+
+     //Data/Input/Prompt user for number of meals to take in hotel
+     printf("\n");
+     printf("\n");
+     printf("\n");
+     printf("How many meals would you like to take in the Hotel?  \n");
+     printf("\n");
+     scanf("%d", &meals);//Read and store number of meals to take in hotel
      getchar();
      system("clear");
 
@@ -85,25 +162,6 @@ int main()
      getchar();
      system("clear");
 
-     //Data/Input/Prompt user for meal information
-     printf("\n");
-     printf("\n");
-     printf("\n");
-     printf("ENTER THE TYPE OF FOOD:\n\nV for Veg\n\nN for Non-Veg\n\nC for Continental \n");
-     printf("\n");
-     scanf("%c", &foodType);//Read and store meal information
-     getchar();
-     system("clear");
-
-     //Data/Input/Prompt user for number of meals to take in hotel
-     printf("\n");
-     printf("\n");
-     printf("\n");
-     printf("How many meals would you like to take in the Hotel?  \n");
-     printf("\n");
-     scanf("%d", &meals);//Read and store number of meals to take in hotel
-     getchar();
-     system("clear");
 
      //Expense Table:
 
@@ -121,19 +179,17 @@ int main()
      printf("***************************************************");
      printf("\n");
      printf("\n");
-     printf("TOTAL EXPENSES TO TRAVEL from %c to %c ", sourceCity, destinationCity);
+     printf("TOTAL EXPENSES TO TRAVEL from %s to %s ", sourceCity1, destinationCity1);
      printf("\n");
      printf("\n");
      printf("***************************************************");
      printf("\n");
      printf("\n");
-     printf("TRANSPORTATION CHARGES (%c) = \n", modeofTranspo);
+     printf("TRANSPORTATION CHARGES (%s) = \n", modeofTranspo1);
      printf("\n");
-     printf("NUMBER OF DAYS OF STAY (%d) = \n", days);
+     printf("NUMBER OF DAYS OF STAY (%s) = %d \n", hotelType1, days);
      printf("\n");
-     printf("TRANSPORTATION CHARGES (%c) = \n", modeofTranspo);
-     printf("\n");
-     printf("NUMBER OF MEALS IN A DAY (%c) = \n", foodType);
+     printf("NUMBER OF MEALS IN A DAY (%s) = %d \n", foodType1, meals);
      printf("\n");
      printf("ROOM CHARGES = \n");
      printf("\n");
