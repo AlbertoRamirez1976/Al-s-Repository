cb85b7461c10436d1a9f9c6a7a1f251d833cb2cb
Alberto E. Ramirez <59782181+AlbertoRamirez1976@users.noreply.github.com>
2021-03-26 03:31:47 -0700

    Add files via upload

--- HW3_PS1_ramirez_a.c
+++ HW3_PS1_ramirez_a.c
@@ -2,61 +2,150 @@
 /* Name : Alberto Ramirez                            */
 /* Student ID : 1186065                              */
 /* Homework 3 Program Set 1                          */
-/* Date: 3/20/21                                     */
-/* This Program calculates the salary raise          */
-/* for employees                                     */
+/* Date: 3/25/21                                     */
+/* This Program calculates employee raise            */
 /*****************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
 
-        //Declaration
-        int iValue, num = 0,count = 0;
-        double salary, rate, raise, newSalary, totalSalary = 0, totalRaise = 0, totalnewSalary = 0;
+    //Declaration
+    float salary,rate,raise,newSalary,totalSalary = 0,totalRaise = 0,totalnewSalary = 0;
 
+    //Header output includes tabed headers and blank line
+    printf("                                  Salary\tRate\tRaise\t New Salary\t\n");
+    printf("---------------------------------------------------------------------------\n");
 
+    //Data/Input will capture user input and feed it to the while loop function
+    printf("Salary: ");
+    scanf("%f", &salary);
 
-            while (salary > 0);
-            {
-            //Data/Input
-            printf("\nEnter salary, or -1 to quit : ");
-            scanf("%lf", &salary);
-            if(salary == -1);
-            return 0;
-
-
-
-            if(salary > 0 && salary < 30000)
-                {
-                    rate = 7.00;
-                }
-                    else if(salary >= 30000 && salary <= 40000)
-                    {
-                        rate = 5.50;
-                    }
-                        else if(salary >= 41000)
-                        {
-                            rate = 4.00;
-                        }
-
-        //Calculations
-        raise = rate * salary / 100;
-        newSalary = salary + raise;
-        totalRaise = totalRaise + raise;
+    //While loop that will continue to loop while salary is true
+    while(salary > 0)
+    {
+        //Calculation of total salary will be used later on to test sentinel value
         totalSalary = totalSalary + salary;
-        totalnewSalary = totalnewSalary + newSalary;
 
-        //Output
+    //If statement to calculate rate which will then be fed into the several calculations for total
+    if(salary > 0 && salary < 30000)
+    {
+        rate = 7.0;
+    }
+    else if(salary >= 30000 && salary <= 40000)
+    {
+        rate = 5.5;
+    }
+    else if(salary > 40000)
+    {
+        rate = 4.0;
+    }
 
-        printf("\n                     Salary\tRate\tRaise\tNew Salary");
-        printf("\n----------------------------------------------------------");
-        printf("\nSalary: %.2lf     %.2lf\t%.2lf\t%.2lf\t% .2lf\t\n",salary, salary, rate, raise, newSalary);
-        printf("\n----------------------------------------------------------");
-        printf("\nTotal                %.2lf\t        %.2lf\t %.2lf\t\n",totalSalary,totalRaise,totalnewSalary);
+    //Calculation will calculate totals for raise, new salary, total raise, and total new salary
+    raise = salary * (rate / 100);//Rate will be divided by 100 in order to move the decimal two places to the left and then the result will be multiplied by salary
+    newSalary = salary + raise;
+    totalRaise = totalRaise + raise;
+    totalnewSalary = totalnewSalary + newSalary;
 
-        return 0;
-        }
+    //Output of salary, rate, raise, new salary
+    printf(" %41.2f %9.2f %10.2f %9.2f\n", salary,rate, raise, newSalary);
 
+    //Data/Input will be capture once again before the end of the while loop.
+    printf("Salary: ");
+    scanf("%f", &salary);
+
+    }
+
+    //If statement to test for sentinel value
+    if(totalSalary <= 0)//If statement will capture the total salary variable from earlier in order to test for sentinel value
+    {
+    //Output of total salary, total raise, total new salary. The total raise variable will be type conversioned to integer according to the HW display requirements
+    printf("---------------------------------------------------------------------------\n");
+    printf("Total           %24.2f %20.2d %13.2f\n", totalSalary, (int)totalRaise, totalnewSalary);
+    }
+    else
+    {
+    //Output of total salary, total raise, total new salary
+    printf("---------------------------------------------------------------------------\n");
+    printf("Total           %27.2f %19.2f %10.2f\n", totalSalary, totalRaise, totalnewSalary);
+    }
+
+    return 0;//End of program which will return back to the main function.
 }
+
+
+//Output
+//
+//Test Run 1
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 25000
+//                                  25000.00      7.00    1750.00  26750.00
+//Salary: 30000
+//                                  30000.00      5.50    1650.00  31650.00
+//Salary: 35000
+//                                  35000.00      5.50    1925.00  36925.00
+//Salary: 40000
+//                                  40000.00      5.50    2200.00  42200.00
+//Salary: -1
+//---------------------------------------------------------------------------
+//Total                             130000.00             7525.00  137525.00
+//
+//
+//Test Run 2
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: -1
+//---------------------------------------------------------------------------
+//Total                               0.00                   00          0.00
+//
+//Test Run 3
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 26000
+//                                  26000.00      7.00    1820.00  27820.00
+//Salary: 31000
+//                                  31000.00      5.50    1705.00  32705.00
+//Salary: 36000
+//                                  36000.00      5.50    1980.00  37980.00
+//Salary: 41000
+//                                  41000.00      4.00    1640.00  42640.00
+//Salary: -1
+//---------------------------------------------------------------------------
+//Total                             134000.00             7145.00  141145.00
+//
+//Test Run 4
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 21000
+//                                  21000.00      7.00    1470.00  22470.00
+//Salary: 39000
+//                                  39000.00      5.50    2145.00  41145.00
+//Salary: 33000
+//                                  33000.00      5.50    1815.00  34815.00
+//Salary: 49000
+//                                  49000.00      4.00    1960.00  50960.00
+//Salary: -1
+//---------------------------------------------------------------------------
+//Total                             142000.00             7390.00  149390.00
+//
+//Test Run 5
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 13000
+//                                  13000.00      7.00     910.00  13910.00
+//Salary: 26000
+//                                  26000.00      7.00    1820.00  27820.00
+//Salary: 34000
+//                                  34000.00      5.50    1870.00  35870.00
+//Salary: 55000
+//                                  55000.00      4.00    2200.00  57200.00
+//Salary: -1
+//---------------------------------------------------------------------------
+//Total                             128000.00             6800.00  134800.00
--- HW3_PS2_ramirez_a.c
+++ HW3_PS2_ramirez_a.c
@@ -2,69 +2,141 @@
 /* Name : Alberto Ramirez                            */
 /* Student ID : 1186065                              */
 /* Homework 3 Program Set 2                          */
-/* Date: 3/20/21                                     */
-/* This Program calculates the salary raise          */
-/* for employees after asking for quantity           */
+/* Date: 3/25/21                                     */
+/* This Program will ask and calculate employee raise*/
 /*****************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
 
-        //Declaration
-        int iValue, num = 0;
-        double salary, rate, raise, newSalary, totalSalary = 0, totalRaise = 0, totalnewSalary = 0;
+    //Declaration
+    int i, num;
+    float salary,rate,raise,newSalary,totalSalary = 0,totalRaise = 0,totalnewSalary = 0;
 
+    //Data/Input will capture user input for the number of salaries to calculate and then feed it to the while loop function
+    printf("How many salaries do you want to enter? ");
+    scanf("%d", &num);
 
+    //Header output includes tabed headers and blank line
+    printf("\n                                  Salary\tRate\tRaise\t New Salary\t\n");
+    printf("---------------------------------------------------------------------------\n");
 
-        while(salary != -1)
-        {
-        //Data/Input
-        printf("\nHow many salaries do you want to enter? ");
-        scanf("%d", &num);
+    for(i = 0; i < num; i++)
+    {
+    //Data/Input will capture user input and feed it to the while loop function
+    printf("Salary: ");
+    scanf("%f", &salary);
 
-        for(iValue = 0; iValue < num; iValue++)
-        {
+    //Calculation of total salary will be used later on to test sentinel value
+    totalSalary = totalSalary + salary;
 
-        printf("\nEnter salary, or -1 to quit : ");
-        scanf("%lf", &salary);
+    //If statement to calculate rate which will then be fed into the several calculations for total
+    if(salary > 0 && salary < 30000)
+    {
+        rate = 7.0;
+    }
+    else if(salary >= 30000 && salary <= 40000)
+    {
+        rate = 5.5;
+    }
+    else if(salary > 40000)
+    {
+        rate = 4.0;
+    }
 
-        if (salary == -1)
-            return 0;
+    //Calculation will calculate totals for raise, new salary, total raise, and total new salary
+    raise = salary * (rate / 100);//Rate will be divided by 100 in order to move the decimal two places to the left and then the result will be multiplied by salary
+    newSalary = salary + raise;
+    totalRaise = totalRaise + raise;
+    totalnewSalary = totalnewSalary + newSalary;
 
-            if(salary > 0 && salary < 30000)
-                {
-                    rate = 7.00;
-                }
-                    else if(salary >= 30000 && salary <= 40000)
-                    {
-                        rate = 5.50;
-                    }
-                        else if(salary >= 41000)
-                        {
-                            rate = 4.00;
-                        }
+    //Output of salary, rate, raise, new salary
+    printf(" %41.2f %9.2f %10.2f %9.2f\n", salary,rate, raise, newSalary);
+    }
 
-        //Calculations
-        raise = rate * salary / 100;
-        newSalary = salary + raise;
-        totalRaise = totalRaise + raise;
-        totalSalary = totalSalary + salary;
-        totalnewSalary = totalnewSalary + newSalary;
+    //Output of total salary, total raise, total new salary
+    printf("---------------------------------------------------------------------------\n");
+    printf("Total           %27.2f %19.2f %10.2f\n", totalSalary, totalRaise, totalnewSalary);
 
-        }
-
-        //Output
-        printf("\n                     Salary\tRate\tRaise\tNew Salary");
-        printf("\n----------------------------------------------------------");
-        printf("\nSalary: %.2lf     %.2lf\t%.2lf\t%.2lf\t% .2lf\t\n",salary, salary, rate, raise, newSalary);
-        printf("\n----------------------------------------------------------");
-        printf("\nTotal                %.2lf\t        %.2lf\t %.2lf\t\n",totalSalary,totalRaise,totalnewSalary);
-
-
-        }
-
-        return 0;
-
+    return 0;//End of program which will return back to the main function.
 }
+
+
+
+//Output
+//
+//Test Run 1
+//
+//How many salaries do you want to enter? 4
+//
+//                                   Salary	      Rate    Raise    New Salary
+//---------------------------------------------------------------------------
+//Salary: 25000
+//                                  25000.00      7.00    1750.00  26750.00
+//Salary: 30000
+//                                  30000.00      5.50    1650.00  31650.00
+//Salary: 35000
+//                                  35000.00      5.50    1925.00  36925.00
+//Salary: 40000
+//                                  40000.00      5.50    2200.00  42200.00
+//---------------------------------------------------------------------------
+//Total                             130000.00             7525.00  137525.00
+//
+//Test Run 2
+//
+//How many salaries do you want to enter? -1
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+//Total                                  0.00                0.00       0.00
+//
+//Test Run 3
+//
+//How many salaries do you want to enter? 3
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 38000
+//                                  38000.00      5.50    2090.00  40090.00
+//Salary: 48000
+//                                  48000.00      4.00    1920.00  49920.00
+//Salary: 58000
+//                                  58000.00      4.00    2320.00  60320.00
+//---------------------------------------------------------------------------
+//Total                             144000.00             6330.00  150330.00
+//
+//Test Run 4
+//
+//How many salaries do you want to enter? 2
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 12000
+//                                  12000.00      7.00     840.00  12840.00
+//Salary: 9000
+//                                   9000.00      7.00     630.00   9630.00
+//---------------------------------------------------------------------------
+//Total                              21000.00             1470.00   22470.00
+//
+//Test Run 5
+//
+//How many salaries do you want to enter? 5
+//
+//                                  Salary	Rate	Raise	 New Salary
+//---------------------------------------------------------------------------
+//Salary: 1256
+//                                   1256.00      7.00      87.92   1343.92
+//Salary: 345682
+//                                 345682.00      4.00   13827.28 359509.28
+//Salary: 7477262525
+//                             7477262336.00      4.00 299090496.00 7776352768.00
+//Salary: 36373
+//                                  36373.00      5.50    2000.52  38373.52
+//Salary: 44
+//                                     44.00      7.00       3.08     47.08
+//---------------------------------------------------------------------------
+//Total                         7477645824.00        299106432.00 7776752128.00
+
